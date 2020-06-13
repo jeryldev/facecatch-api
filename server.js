554d4b5f17d0eeb0a9354d429fd4ce305c1f434d
JerylDEv <jeryldev@gmail.com>
2020-06-13 22:45:15 +0800

    updated the port dynamically

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.put('/image', (req, res) => image.handleImage(req, res, db));
 // Post - /imageurl
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
-app.listen(4000, () => {
-  console.log('app is running on port 4000');
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`app is running on port ${process.env.PORT || 4000}`);
 });
