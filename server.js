d27472dd463cbcb8a19544ad705d6bfec78e12bd
JerylDEv <jeryldev@gmail.com>
2020-06-14 00:38:21 +0800

    updated connectionString

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import image from './controllers/image.js';
 const db = knex({
   client: 'pg',
   connection: {
-    connectString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
